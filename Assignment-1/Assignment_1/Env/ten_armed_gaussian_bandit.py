4b306e1f4706d8b2d961db183a55a67f4f2c06c0
Romit Mohanty <romitkt8@gmail.com>
2021-09-11 20:58:20 +0530

    final

--- Assignment-1/Assignment_1/Env/ten_armed_gaussian_bandit.py
+++ Assignment-1/Assignment_1/Env/ten_armed_gaussian_bandit.py
@@ -5,21 +5,23 @@ import numpy as np
 import random
 from scipy.stats import bernoulli
 class ten_armed_gaussian_bandit(Env):
-    def __init__(self):
+    def __init__(self,mu,sigma,arms):
         # action space left =0, right=1;
         self.action_space = Discrete(10)
         # state spaces are 1 -non-terminal, 0,2 terminal
         self.observation_space=Discrete(11)
         # initial state is 1
         self.state=0
-       
+        self.mu=mu
+        self.sigma=sigma
+        self.arms=arms
         # mean rewards of each bandit 
-        self.mean_rewards=np.random.normal(0,1,10)
+        self.mean_rewards=np.random.normal(mu,sigma,arms)
         # no. of steps per episode= 1 greedy horizon
 
     def step(self,action):
         # rewards to be sample from a gaussian distribution
-        reward = np.random.normal(self.mean_rewards[action],1,1)[0]
+        reward = np.random.normal(self.mean_rewards[action],self.sigma,1)[0]
         self.state=action+1      
         done= True
         info={}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 My solutions to assignment-1 of DRL-CS698R under Prof. Ashutosh Modi, IIT Kanpur
 All the values are found setting a single global random seed set to 3.
 This contains all the plots obtained for different questions as well as some extra plots generated for comparision
-
+The file Nomenclatures are self explanatory. I have made 3 notebooks pertaining to different environments i.e. two armed bernoulli bandit, ten armed gaussian bandit, random walk.
